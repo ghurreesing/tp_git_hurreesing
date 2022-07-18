43b5f84eb35b2923cae36aa9ace7746ac14f8155
ghurreesing <gahurreesing@student.udm.ac.mu>
2022-07-18 19:56:45 +0400

    [add] modifying python file to display error message when arguments entered less than 2 or more than 2

--- addTwoInt.py
+++ addTwoInt.py
@@ -2,14 +2,29 @@
  
 import sys
 
+if len(sys.argv) == 1:
+  print ('Error No arguments entered: Provide two arguments')
+  sys.exit()
+
+if len(sys.argv) == 2:
+  print ('Error one argument entered: Provide two arguments')
+  sys.exit()
+
+if len(sys.argv) >= 4:
+  print ('Error more than two  arguments entered: Provide only two arguments')
+  sys.exit()
+
+
 print(sys.argv)
 
 a=int(sys.argv[1])
 b=int(sys.argv[2])
 
+
 def add(a,b):
   sum=a+b
   return sum 
 
-
 print("The sum of the two values is: ",add(a,b))
+
+
